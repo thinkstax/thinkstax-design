3331fcf6b5a8447572d068f1374913b9c444830b
Markus Daehn <markus.daehn@gmail.com>
2018-07-12 22:56:10 -0700

    Remove unnecessary constants
    Remove unnecessary constants

--- constants.js
+++ constants.js
@@ -12,39 +12,18 @@ export const breakpoints = createEnum({
 });
 
 export const themeModes = createEnum({
-  TURBO_TAX_ONLINE: 'tto'
-  // QUICKBOOKS_ONLINE: 'qbo'
+  DARK: 'dark'
 });
 
-export const CHANNELS_DEFAULT = 'CALLBACK';
-export const channels = createEnum({
-  ANSWER_EXCHANGE: 'axc',
-  [CHANNELS_DEFAULT]: 'callBack',
-  PHONE: 'phone',
-  SCHEDULING: 'scheduling',
-  UPSELL: 'upsell'
-}, CHANNELS_DEFAULT);
-
-export const CHANNEL_PRIORITY = Object.freeze([
-  channels.CALLBACK,
-  channels.SCHEDULING,
-  channels.PHONE
-]);
-
-export const CHANNELS_TEXT_RESOURCE_KEY = 'channels';
 /** naming convention: constant objects names are uppercased with keys camelcased */
 export const THEME_DEFAULT = Object.freeze({
-  mode: themeModes.TURBO_TAX_ONLINE,
+  mode: themeModes.DARK,
   breakpoint: breakpoints.EXTRA_SMALL
 });
 
 export const THEME_PROPTYPE = PropTypes.shape({
   mode: PropTypes.oneOf(themeModes.values).isRequired,
   breakpoint: PropTypes.oneOf(breakpoints.values).isRequired
-});
-
-export const textModifiers = createEnum({
-  HIGHLIGHT: 'highlight'
 });
 
 export const EMPTY_STRING = '';
@@ -56,10 +35,6 @@ export default {
   THEME_DEFAULT,
   THEME_PROPTYPE,
   themeModes,
-  CHANNELS_DEFAULT,
-  CHANNELS_TEXT_RESOURCE_KEY,
-  channels,
   breakpoints,
-  textModifiers
 };
 
