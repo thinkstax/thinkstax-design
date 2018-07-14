19cabeb1beb8adcc3a0b6a3d5ac5519cad4c039a
Markus Daehn <markus.daehn@gmail.com>
2018-07-13 20:20:13 -0700

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Button from './Button';
+import CheckBox from './CheckBox';
+import TextInput from './TextInput';
+import Collapse from './Collapse';
+
+export { Button, CheckBox, TextInput, Collapse };
+
+export default {
+  Button,
+  CheckBox,
+  TextInput,
+  Collapse
+};
